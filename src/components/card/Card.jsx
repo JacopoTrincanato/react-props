e3058b5fe7f61fd859a46bf4b27d3895c84e5664
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-13 15:55:16 +0100

    insert post props in Card component and add section with tags

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,28 +4,22 @@ import style from './Card.module.css'
 //importo il bottone
 import Button from '../button/Button';
 
-//creo il componente Card
-export default function Card() {
-    //creo una variabile per il testo che indica la dimensione della card
-    const cardSize = '600x400';
-
-    //creo una variabile per il titolo della card
-    const cardTitle = 'Titolo del post';
-
-    //creo una variabile per la descrizione della card
-    const cardDescription = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum magnam autem consectetur pariatur reiciendis a, vero voluptate ad modi earum et nisi quia possimus, numquam adipisci magni delectus quasi ab.';
+//creo il componente Card e gli aggiungo la props post
+export default function Card({ post }) {
 
     //eseguo il return
     return (
 
         <div className={style.card}>
             <div className={`${style.cardTop} ${style.dFlex}`}>
-                <p>{cardSize}</p>
+                <p>{post.image}</p>
             </div>
 
             <div className={style.cardBottom}>
-                <h3 className={style.mt1}>{cardTitle}</h3>
-                <p className={style.mt1}>{cardDescription}</p>
+                <h3 className={style.mt1}>{post.title}</h3>
+                <p className={style.mt1}>{post.content}</p>
+
+                <section className={style.mt1}>{post.tags}</section>
                 <Button />
             </div>
         </div>
