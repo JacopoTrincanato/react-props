7fabec3620793fae93a2a8e30c5843b4480c7267
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-13 17:23:46 +0100

    create a section with tags in main in AppMain.jsx

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -6,13 +6,36 @@ import posts from "../data/posts"
 
 //creo il componente main
 export default function AppMain() {
+    //ciclo all'interno dell'array posts per creare una sezione con i tag in modo che non si ripetano
 
+    //creo un array vuoto
+    const uniqueTags = [];
+
+    //ciclo all'interno di post
+    posts.forEach(post => {
+
+        //ciclo all'interno dei tag
+        post.tags.forEach(tag => {
+            if (!uniqueTags.includes(tag)) {
+                uniqueTags.push(tag);
+            }
+        })
+
+    });
 
     //eseguo il return
     return (
         <main>
 
             {posts.map(post => <Card key={post.id} post={post} />)}
+
+            <section>
+                {uniqueTags && uniqueTags.map((tag, index) => (
+                    <span key={index}>
+                        {tag}{index < uniqueTags.length - 1 ? ', ' : ''}
+                    </span>
+                ))}
+            </section>
         </main>
 
     )
